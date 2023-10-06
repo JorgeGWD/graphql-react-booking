554d6a53de0505949d173ce8846fce7f78e7de54
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-10-06 16:02:37 -0500

    Install and configure graphql

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const expressGraphQL = require('express-graphql').graphqlHTTP
+const  { buildSchema } = require('graphql')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World')
-})
+app.use('/graphql', expressGraphQL({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Coding all night']
+        },
+        createEvent: (args) => {
+            const eventName = args.eventName
+            return eventName
+        }
+    },
+    graphiql: true
+}))
 
 app.listen(3000)
