394d6e2e9e29db1dea2778532a1b3127a436e064
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-10-17 11:03:35 -0500

    Gramal corrections

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,15 +3,32 @@ const bcrypt = require('bcryptjs')
 const Event = require('../../models/event')
 const User = require('../../models/user')
 const Booking = require('../../models/booking')
-const booking = require('../../models/booking')
 
 const events = async eventIds => {
     try{
         const events = await Event.find({_id: {$in: eventIds}})
         events.map(event => {
-            return {...event._doc, id: event.id, date: new Date(event._doc.date).toISOString(), creator: user.bind(this, event.creator)}
+            return {
+                ...event._doc,
+                id: event.id,
+                date: new Date(event._doc.date).toISOString(),
+                creator: user.bind(this, event.creator)
+            }
         })
         return events
+    } catch (err) {
+        throw err
+    }
+}
+
+const singleEvent = async eventId => {
+    try {
+        const event = await Event.findById(eventId)
+        return {
+            ...event._doc,
+            _id: event.id,
+            creator: user.bind(this, event.creator)
+        }
     } catch (err) {
         throw err
     }
@@ -20,7 +37,11 @@ const events = async eventIds => {
 const user = async userId => {
     try {
         const user = await User.findById(userId)
-        return {...user._doc, id: user.id, createdEvents: events.bind(this, user._doc.createdEvents)}
+        return {
+            ...user._doc,
+            id: user.id,
+            createdEvents: events.bind(this, user._doc.createdEvents)
+        }
     } catch (err) {
         throw err
     }
@@ -31,7 +52,12 @@ module.exports = {
         try {
             const events = await Event.find()
             return events.map(event => {
-                return {...event._doc, _id: event.id, date: new Date(event._doc.date).toISOString(), creator: user.bind(this, event._doc.creator)}
+                return {
+                    ...event._doc,
+                    _id: event.id,
+                    date: new Date(event._doc.date).toISOString(),
+                    creator: user.bind(this, event._doc.creator)
+                }
             })
         } catch (err) {
             throw err
@@ -41,7 +67,14 @@ module.exports = {
         try {
             const bookings = await Booking.find()
             return bookings.map(booking => {
-                return {...booking._doc, _id: booking.id, createdAt: new Date(booking._doc.createdAt).toISOString(), updatedAt: new Date(booking._doc.updatedAt).toISOString()}
+                return {
+                    ...booking._doc,
+                    _id: booking.id,
+                    user: user.bind(this, booking._doc.user),
+                    event: singleEvent.bind(this, booking._doc.event),
+                    createdAt: new Date(booking._doc.createdAt).toISOString(),
+                    updatedAt: new Date(booking._doc.updatedAt).toISOString()
+                }
             })
         } catch (err) {
             throw err
@@ -53,13 +86,18 @@ module.exports = {
             description: args.eventInput.description,
             price: +args.eventInput.price,
             date: new Date(args.eventInput.date),
-            creator: '65242af2215a32501907d676'
+            creator: '652469dc114352cd0d913ad0'
         })
         let createdEvent
         try {
             const result = await event.save()
-            createdEvent = {...result._doc, _id: event.id, date: new Date(event._doc.date).toISOString(), creator: user.bind(this, result._doc.creator)}
-            const creator = await User.findById('65242af2215a32501907d676')
+            createdEvent = {
+                ...result._doc,
+                _id: result._doc._id.toString(),
+                date: new Date(event._doc.date).toISOString(),
+                creator: user.bind(this, result._doc.creator)
+            }
+            const creator = await User.findById('652469dc114352cd0d913ad0')
             if(!creator) {
                 throw new Error('User not found.')
             }
@@ -82,7 +120,11 @@ module.exports = {
                     password: hashedPassword
                 })
                 const result = await user.save()
-                return {...result._doc, password: null, _id: result.id}
+                return {
+                    ...result._doc,
+                    password: null,
+                    _id: result.id
+                }
         } catch (err) {
             throw err
         }
@@ -90,10 +132,31 @@ module.exports = {
     bookEvent: async args => {
         const fetchedEvent = await Event.findOne({_id: args.eventId})
         const booking = new Booking({
-            user: '65242af2215a32501907d676',
+            user: '652469dc114352cd0d913ad0',
             event: fetchedEvent
         })
         const result = await booking.save()
-        return {...result._doc, _id: result.id, createdAt: new Date(result._doc.createdAt).toISOString(), updatedAt: new Date(result._doc.updatedAt).toISOString()}
+        return {
+            ...result._doc,
+            _id: result.id,
+            user: user.bind(this, booking._doc.user),
+            event: singleEvent.bind(this, booking._doc.event),
+            createdAt: new Date(result._doc.createdAt).toISOString(),
+            updatedAt: new Date(result._doc.updatedAt).toISOString()
+        }
+    },
+    cancelBooking: async args => {
+        try {
+            const booking = await Booking.findById(args.bookingId).populate('event')
+            const event = {
+                ...booking.event._doc,
+                _id: booking.event.id,
+                creator: user.bind(this, booking.event._doc.creator)
+            }
+            await Booking.deleteOne({_id: args.bookingId})
+            return event
+        } catch (err) {
+            throw err
+        }
     }
 }
--- models/booking.js
+++ models/booking.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const bookingSchema = new Schema({
-    evetns: {
+    evetn: {
         type: Schema.Types.ObjectId,
         ref: 'Event'
     },
