fe295ddb5d5f149128900190d3c1665e3ca4ce0b
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-10-27 13:17:39 -0500

    Handle input data on submit

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,23 +1,38 @@
-import React from 'react'
-import './Auth.css'
+import { useState } from 'react';
+import './Auth.css';
 
 const AuthPage = () => {
-  return (
-    <form className='auth-form'>
-      <div className='auth-form__control'>
-        <label htmlFor='email'>E-mail</label>
-        <input type='email' id='email' />
-      </div>
-      <div className='auth-form__control'>
-        <label htmlFor='password'>Password</label>
-        <input type='password' id='password' />
-      </div>
-      <div className='auth-form__actions'>
-        <button type='submit'>Submit</button>
-        <button type='button'>Switch to Singup</button>
-      </div>
-    </form>
-  )
-}
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-export default AuthPage
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        if (email.trim().length === 0 || password.trim().length === 0) {
+        return;
+        }
+
+        console.log(email, password);
+    };
+
+    return (
+        <form className='auth-form' onSubmit={submitHandler}>
+        <div className='auth-form__control'>
+            <label htmlFor='email'>E-mail</label>
+            <input type='email' id='email' value={email} onChange={(e) => setEmail(e.target.value)}
+            />
+        </div>
+        <div className='auth-form__control'>
+            <label htmlFor='password'>Password</label>
+            <input type='password' id='password' value={password} onChange={(e) => setPassword(e.target.value)}
+            />
+        </div>
+        <div className='auth-form__actions'>
+            <button type='submit'>Submit</button>
+            <button type='button'>Switch to Signup</button>
+        </div>
+        </form>
+    );
+};
+
+export default AuthPage;
