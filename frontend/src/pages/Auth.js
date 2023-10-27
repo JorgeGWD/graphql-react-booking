bca6cc906b71e69b8b5cabd09d65d71dc3854463
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-10-27 12:45:58 -0500

    Add a form to loging in Auth file, an agregate some styles

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './Auth.css'
 
 const AuthPage = () => {
   return (
-    <h1>The Auth Page</h1>
+    <form className='auth-form'>
+      <div className='auth-form__control'>
+        <label htmlFor='email'>E-mail</label>
+        <input type='email' id='email' />
+      </div>
+      <div className='auth-form__control'>
+        <label htmlFor='password'>Password</label>
+        <input type='password' id='password' />
+      </div>
+      <div className='auth-form__actions'>
+        <button type='submit'>Submit</button>
+        <button type='button'>Switch to Singup</button>
+      </div>
+    </form>
   )
 }
 
